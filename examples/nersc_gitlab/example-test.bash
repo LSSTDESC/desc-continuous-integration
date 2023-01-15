22d0a05431e437dd56326ae08d9c925db91b8498
Stuart McAlpine <stuart.mcalpine@helsinki.fi>
2023-01-15 14:04:09 +0100

    Added mydescpackage install  to nersc example

--- examples/nersc_gitlab/example-test.bash
+++ examples/nersc_gitlab/example-test.bash
@@ -7,5 +7,8 @@ echo "Running at NERSC on" $NERSC_HOST
 # Load desc-python Conda environment.
 source /global/common/software/lsst/common/miniconda/setup_current_python.sh
 
+# Install mydescpackage package.
+python3 -m pip install .
+
 # Run tests.
 pytest ./tests
