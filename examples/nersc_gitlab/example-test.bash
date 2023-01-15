9876cb79bc5174037f85237e225a94a2b42b2e82
Stuart McAlpine <stuart.mcalpine@helsinki.fi>
2023-01-15 14:19:11 +0100

    Updated NERSC example

--- examples/nersc_gitlab/example-test.bash
+++ examples/nersc_gitlab/example-test.bash
@@ -7,8 +7,10 @@ echo "Running at NERSC on" $NERSC_HOST
 # Load desc-python Conda environment.
 source /global/common/software/lsst/common/miniconda/setup_current_python.sh
 
-# Install mydescpackage package.
-python3 -m pip install .
+# Install mydescpackage package in a venv.
+python3 -m venv my_ci_venv
+source my_ci_venv/bin/activate
+python3 -m pip install .[ci]
 
 # Run tests.
 pytest ./tests
